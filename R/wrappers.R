d0a24d7ac8165bcfeb7b91ec05e8bf4db26e5e1c
Chaitanya Inumella <cinumell@asu.edu>
2022-01-24 08:01:25 -0700

    Added the few checks for the APIs

--- R/wrappers.R
+++ R/wrappers.R
@@ -81,29 +81,29 @@ NULL
 # }
 # 
 # # Matrix Multiplication
-# gpu_Mat_mult <- function(A, B){
-# 	
-# 	type <- typeof(A)
-# 	
-# 	C <- cudaMatrix(nrow=nrow(A), ncol=ncol(B), type=type)
-# 	
-# 	switch(type,
-# 				 integer = {stop("integer not currently implemented")},
-# 				 float = {cpp_vienna_cudaMatrix_sgemm(A@address,
-# 				 																		B@address,
-# 				 																		C@address)
-# 				 },
-# 				 double = {
-# 				 	if(!deviceHasDouble()){
-# 				 		stop("Selected GPU does not support double precision")
-# 				 	}else{cpp_vienna_cudaMatrix_dgemm(A@address,
-# 				 																	 B@address,
-# 				 																	 C@address)
-# 				 	}
-# 				 },
-# 				{
-# 					stop("type not recognized")
-# 				})
-# 	
-# 	return(C)
-# }
+gpu_Mat_mult <- function(A, B){
+ 	
+ 	type <- typeof(A)
+ 	
+ 	C <- cudaMatrix(nrow=nrow(A), ncol=ncol(B), type=type)
+ 	
+ 	switch(type,
+ 				 integer = {stop("integer not currently implemented")},
+ 				 float = {cpp_vienna_cudaMatrix_sgemm(A@address,
+ 				 																		B@address,
+ 				 																		C@address)
+ 				 },
+ 				 double = {
+ 				 	if(!deviceHasDouble()){
+ 				 		stop("Selected GPU does not support double precision")
+ 				 	}else{cpp_vienna_cudaMatrix_dgemm(A@address,
+ 				 																	 B@address,
+ 				 																	 C@address)
+ 				 	}
+ 				 },
+ 				{
+ 					stop("type not recognized")
+ 				})
+ 	
+ 	return(C)
+}
